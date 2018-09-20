d9a1680b778d3eb8d598ef30c2dd3761e55d8aab
ishaanm <ishaanm@thoughtworks.com>
2018-09-20 17:32:25 +0530

    im| #000 Fixing  issues

--- reservationdetails/reservation/src/main/java/com/vodqa/reservation/repos/PassengerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vodqa.reservation.repos;
-
-import com.vodqa.reservation.entities.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-
-}
--- reservationdetails/reservation/src/main/java/com/vodqa/reservation/repos/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vodqa.reservation.repos;
-
-import com.vodqa.reservation.entities.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-}
